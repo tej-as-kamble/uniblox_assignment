8a8d95d87f3896046b10a428301ef4b114a4344d
tej-as-kamble <tejaskk13@gmail.com>
2025-05-17 09:29:06 +0530

    Fix spelling mistake

--- Backend/routes/itemRoutes.js
+++ Backend/routes/itemRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Item = require('../models/item');
+const Item = require('../models/Item');
 
 
 router.get('/', async (req, res) => {
